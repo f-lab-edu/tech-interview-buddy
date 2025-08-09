d195c513cfadd86de02aa8d9b25ea57c8df822d6
daydream-03 <byjung1230@gmail.com>
2025-08-09 21:11:22 +0900

    Apply Lombok to DTOs and User Entity

--- src/main/java/com/example/tech_interview_buddy/dto/request/UserCreateRequest.java
+++ src/main/java/com/example/tech_interview_buddy/dto/request/UserCreateRequest.java
@@ -1,11 +1,18 @@
 package com.example.tech_interview_buddy.dto.request;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCreateRequest {
     @NotBlank(message = "Username is required")
     private String username;
@@ -17,17 +24,4 @@ public class UserCreateRequest {
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
-
-    // Getters
-    public String getUsername() {
-        return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
