f9ffcb1143a695f631e9643fc96ba21e82a32674
daydream-03 <byjung1230@gmail.com>
2025-08-10 20:54:13 +0900

    Update UserLoginResponse to return JWT token instead of user info

--- src/main/java/com/example/tech_interview_buddy/dto/response/UserLoginResponse.java
+++ src/main/java/com/example/tech_interview_buddy/dto/response/UserLoginResponse.java
@@ -2,10 +2,11 @@ package com.example.tech_interview_buddy.dto.response;
 
 import lombok.Getter;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserLoginResponse {
-    private String username;
-    private String email;
+    private String token;
 }
