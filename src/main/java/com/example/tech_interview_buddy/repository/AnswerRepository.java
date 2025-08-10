062fba5c0bd9fca3498053f850a6eee1f14a3b2d
daydream-03 <byjung1230@gmail.com>
2025-08-10 17:14:57 +0900

    Add AnswerRepository

---
+++ src/main/java/com/example/tech_interview_buddy/repository/AnswerRepository.java
@@ -0,0 +1,15 @@
+package com.example.tech_interview_buddy.repository;
+
+import com.example.tech_interview_buddy.domain.Answer;
+import com.example.tech_interview_buddy.domain.User;
+import com.example.tech_interview_buddy.domain.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    
+    Optional<Answer> findByUserAndQuestion(User user, Question question);
+} 
