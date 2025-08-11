9351103dc1e18a11e912c59c23683fb5999ca920
daydream-03 <byjung1230@gmail.com>
2025-08-11 23:25:34 +0900

    Remove validation annotations from DTOs

--- src/main/java/com/example/tech_interview_buddy/dto/request/UserCreateRequest.java
+++ src/main/java/com/example/tech_interview_buddy/dto/request/UserCreateRequest.java
@@ -5,23 +5,12 @@ import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserCreateRequest {
-    @NotBlank(message = "Username is required")
     private String username;
-
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email address")
     private String email;
-
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 }
