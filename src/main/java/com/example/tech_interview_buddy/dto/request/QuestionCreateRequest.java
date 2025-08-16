2c5d98858ff480cfd4b76aa11100f41b6d27529a
daydream-03 <byjung1230@gmail.com>
2025-08-16 23:54:45 +0900

    Create AdminService and related DTOs for admin functionality

---
+++ src/main/java/com/example/tech_interview_buddy/dto/request/QuestionCreateRequest.java
@@ -0,0 +1,17 @@
+package com.example.tech_interview_buddy.dto.request;
+
+import com.example.tech_interview_buddy.domain.Category;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class QuestionCreateRequest {
+    private String content;
+    private Category category;
+    private List<String> tags;
+}
