86b22c299aab52887e92707e016d478b611fc6b1
daydream-03 <byjung1230@gmail.com>
2025-11-28 00:51:56 +0900

    Add common module for shared utilities

---
+++ common/src/main/java/com/example/tech_interview_buddy/common/domain/Category.java
@@ -0,0 +1,13 @@
+package com.example.tech_interview_buddy.common.domain;
+
+public enum Category {
+    PROGRAMMING,
+    FRAMEWORK,
+    DATABASE,
+    INFRASTRUCTURE,
+    ALGORITHM,
+    SYSTEM_DESIGN,
+    NETWORK,
+    SECURITY,
+    DEVOPS
+}
