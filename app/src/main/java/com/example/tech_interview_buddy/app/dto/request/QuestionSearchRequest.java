9283de110e7e40ddac1d9a3fdd8c13eca7e71c4f
daydream-03 <byjung1230@gmail.com>
2026-02-05 14:39:35 +0900

    Remove unnecessary comments

--- app/src/main/java/com/example/tech_interview_buddy/app/dto/request/QuestionSearchRequest.java
+++ app/src/main/java/com/example/tech_interview_buddy/app/dto/request/QuestionSearchRequest.java
@@ -28,13 +28,3 @@ public class QuestionSearchRequest {
     @Builder.Default
     private SortDirection direction = SortDirection.ASC;
 }
-
-// 1. 테스트 코드 (QuestionRepository, utils쪽 builder 관련)
-// TDD
-// JUnit, Jupiter >>> Spock 선호
-// 리파지토리 테스트는 통합 테스트이다.
-// 각 테스트 별 격리가 필요. DB 작업이 필요 (DB 유닛: XML로 테이블의 데이터를 관리할 수 있다. 각 테스트 메서드마다 DB
-// 테스트 용 DB는 별도로 분리해야 한다.
-// 2. 요청 분석
-// 그라파나, 로그백
-//
